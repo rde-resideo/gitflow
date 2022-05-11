bae1c542f587a8d91df6fde0d5e7b21b011573e1
Mahendra Jena <53595991+mahendrajena@users.noreply.github.com>
2022-05-11 12:55:45 +0530

    Create myapp.java

---
+++ myapp.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class MyFirstClass
+{
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        int a;
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        System.out.println((a+10));
+        //s.close();
+    }
+}
