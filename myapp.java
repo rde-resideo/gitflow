ac6698c9c18418bfc920aa9c2d8446b7dcb7eca1
jmahen <30362646+jmahen@users.noreply.github.com>
2022-05-11 19:38:45 +0530

    Update myapp.java

--- myapp.java
+++ myapp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class MyFirstClass
 {
     public static void main(String[] args) {
-        System.out.println("Enter A Number-U1");
+        System.out.println("Enter A Number-U2");
         int a;
         Scanner s=new Scanner(System.in);
         a=s.nextInt();
